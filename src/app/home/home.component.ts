7ad485bcf7780f46d3870ebc459e8031f2864d5f
hegdeg <Ganapati.Hegde@viacomcontractor.com>
2022-02-28 00:36:35 +0530

    fixes as of 02_28_2022

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import {LoginComponent} from '../login/login.component';
 import {SignupComponent} from '../signup/signup.component';
+/* import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+import { SocialAuthService } from "angularx-social-login"; */
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,6 @@ import {SignupComponent} from '../signup/signup.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   constructor( private router: Router, private dialog: MatDialog) { }
 
   ngOnInit(): void {
@@ -18,21 +19,42 @@ export class HomeComponent implements OnInit {
 
   openLogInDialoge(){
 
-    const dialogRef = this.dialog.open(LoginComponent, {
-      position: {
-        top: '100px',
-        left: '450px'
-      }
-    });
-   
+    if(!localStorage.getItem('user')){
+      const dialogRef = this.dialog.open(LoginComponent, {
+        position: {
+          top: '100px',
+          left: '450px'
+        }
+      });
+  
+    }
+    else 
+    this.router.navigate(['matches']);
+    
   }
   openSignUpDialoge(){
+    if(!localStorage.getItem('user')){
     this.dialog.open(SignupComponent, {
       position: {
         top: '0px',
         left: '450px'
       }
     });
+  } else
+  this.router.navigate(['matches']);
   }
+
+/*   signInWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.socialAuthService.signOut();
+  } */
+
+/*   loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then(() => this.router.navigate(['introi']));
+  } */
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 import { ErrorStateMatcher } from '@angular/material/core';
 import {MatDialogRef} from '@angular/material/dialog';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { validateBasis } from '@angular/flex-layout';
+import { from } from 'rxjs';
+
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
@@ -16,16 +20,18 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+public socialUser: SocialUser = new SocialUser;
 loginForm: FormGroup;
 matcher = new MyErrorStateMatcher();
 isLoadingResults = false;
-
+isLoginError:boolean = false;
 
   hide = true;
-  
+
   //emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
   constructor(private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService,
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthenticationService,
@@ -33,13 +39,20 @@ isLoadingResults = false;
 
   async ngOnInit() {
 
+    this.socialAuthService.authState.subscribe((user)=>{
+      this.socialUser = user;
+      console.log("From GOOGLE"+JSON.stringify(user));
+      console.log("MY APP FR GOOGLE"+JSON.stringify(this.socialUser));
+    });
+    
     if(localStorage.getItem('token')){
       this.router.navigate(['matches']);
   }
-    
+
     this.loginForm = this.formBuilder.group({
       emailOrPhone: [null, [Validators.required, Validators.pattern('^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})|([0-9]{10})+$')]],
-      password: [null, Validators.required]
+      password: [null, Validators.required],
+      loginFrom: "Madwe"
     });
   }
 
@@ -49,22 +62,71 @@ isLoadingResults = false;
       return;
     }
  */
+    
     this.authService.login(form)
       .subscribe(res => {
         console.log(res);
         if (res.token) {
           localStorage.setItem('token', res.token);
           localStorage.setItem("user",JSON.stringify(res.user));
-          this.router.navigate(['matches']);
+          this.dialogRef.close();
+          this.router.navigate(['introi']);
+        } else {
+          this.isLoginError=true;
         }
       }, (err) => {
         console.log(err);
       });
-      this.dialogRef.close();
+      /* this.dialogRef.close(); */
+  }
+
+  loginWithGoogle(){
+    /* this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID); */
+    if(!localStorage.getItem("user")){
+      this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+    .then(() => this.loginAndGenerateTokenForGoogleUser());
+    }
+    
+  
+    /* from(this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)).subscribe((user)=>{
+      this.socialUser = user;
+      console.log("From GOOGLE"+JSON.stringify(user));
+      console.log("MY APP FR GOOGLE"+JSON.stringify(this.socialUser));
+    }) */
+  /*  this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      console.log("From GOOGLE"+user);
+      console.log("MY APP FR GOOGLE"+this.socialUser);
+    }); */
+   
   }
 
   cancel() {
     this.router.navigate(['home']);
+  }
+
+  loginAndGenerateTokenForGoogleUser(){
+    this.socialAuthService.authState.subscribe((user)=>{
+      this.socialUser = user;
+      console.log("From GOOGLE"+JSON.stringify(user));
+      console.log("MY APP FR GOOGLE"+JSON.stringify(this.socialUser));
+    });
+    this.loginForm.get('emailOrPhone').setValue(this.socialUser.email);
+    this.loginForm.get('loginFrom').setValue("Google");
+    this.authService.login(this.loginForm.value)
+      .subscribe(res => {
+        console.log(res);
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+          localStorage.setItem("user",JSON.stringify(res.user));
+          this.dialogRef.close();
+          this.router.navigate(['matches']);
+        } else {
+          this.isLoginError=true;
+        }
+      }, (err) => {
+        console.log(err);
+      });
   }
 
 }
